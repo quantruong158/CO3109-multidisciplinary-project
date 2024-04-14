4989e8f770e818c155ad0a08a4fc25625d87815f
quantruong158 <quantruong150803@gmail.com>
2024-04-14 18:08:42 +0700

    Fix escape character

--- src/components/component/login.tsx
+++ src/components/component/login.tsx
@@ -70,7 +70,7 @@ export function Login() {
             </Button>
           </div>
           <div className='mt-4 text-center text-sm'>
-            Don't have an account?{' '}
+            Don&apos;t have an account?{' '}
             <Link className='underline' href='/signup'>
               Sign up
             </Link>
